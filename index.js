6b50604d365c977a3db46eb8cc69e55f3fc447ec
Joseph <joseph.dena@student.moringaschool.com>
2023-10-22 17:33:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) } {
+    return "This one is on me!";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+    console.log("You get a free sample");
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  } 
+  else if ((ride >= 2000) && (ride <= 2500)) {
+    return "I will gladly take your thirty bucks.";
+
+  } else if (ride > 2500) {
+    return "No can do.";
+  }
+
+
+  function ternaryCheckCity(city) {
+    // Using the ternary operator to check if the city is 'NYC' and return the appropriate message
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+
+
+function switchOnCharmFromTip() {
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
 }
